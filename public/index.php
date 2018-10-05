f1b79c4030e0c14e2684bee93399c455e28551ea
yunjan <yt266@njit.edu>
2018-10-05 13:54:24 -0400

    Task: csv file setup

--- public/index.php
+++ public/index.php
@@ -6,6 +6,38 @@
  * Time: 8:10 PM
  */
 
-echo'test';
-echo'12345test';
-echo'hello world';
+main::start();
+
+class main {
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+
+    }
+}
+class csv {
+
+    static public function getRecords() {
+
+    }
+}
+
+class html{
+
+    static public function generateTable($records) {
+
+        $table = $records;
+
+        return $table;
+    }
+}
+
+class system{
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+
+}
