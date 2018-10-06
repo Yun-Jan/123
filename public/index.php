7c42a9bc4028e8db432d7e276ce43ef2d4062fb7
yunjan <yt266@njit.edu>
2018-10-05 21:21:26 -0400

    Feature: Added recordFactory function

--- public/index.php
+++ public/index.php
@@ -5,24 +5,47 @@
  * Date: 10/5/18
  * Time: 7:21 PM
  */
-main::start();
+main::start("example.csv");
 
 class main{
-    static public function start() {
+    static public function start($filename){
 
-        $file= fopen("example.csv","r");
-        while(! feof($file))
-        {
-         $record =fgetcsv($file);
+        $records = csv::getRecords($filename);
 
-         $records[]=$record;
-        }
-
-        fclose($file);
-        print_r($records);
     }
 }
 
 class csv{
-    static public function
+    static public function getRecords($filename){
+        $file = fopen($filename,"r");
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+
+            $records[] = recordFactory::create($record);
+        }
+
+        fclose($file);
+        return $records;
+    }
+}
+
+class record{
+    public function __construct(Array $record= null)
+    {
+        $this ->createProperty();
+        print_r($this);
+    }
+    public function createProperty($name = 'first', $value='keith'){
+        $this->{$name} = $value;
+    }
+}
+
+class recordFactory{
+    static public function create(Array $array= null) {
+        $record = new record($array);
+        return $record;
+}
+
 }
