35683cba36d2987c39e7ef6286e448e45b792e56
yunjan <yt266@njit.edu>
2018-10-06 09:42:17 -0400

    Task: Added read csv function

--- public/index.php
+++ public/index.php
@@ -5,11 +5,24 @@
  * Date: 10/6/18
  * Time: 9:05 AM
  */
-main::start();
+main::start("example.csv");
 
-class main {
-    static public function start(){
-        $file = fopen("example.csv", "r");
+class main
+{
+    static public function start($filename)
+    {
+        $records = csv::getRecords($filename);
+        print_r($records);
+
+    }
+}
+
+
+class csv {
+
+    static public function getRecords($filename){
+
+        $file = fopen($filename, "r");
         while(! feof($file)){
 
             $record = fgetcsv($file);
@@ -17,6 +30,6 @@ class main {
         }
 
         fclose($file);
-        print_r($records);
+        return $records;
     }
 }
