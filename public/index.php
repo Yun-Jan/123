28fc4894c638ef004a3197be9382dedda8983b8a
yunjan <yt266@njit.edu>
2018-10-05 21:59:55 -0400

    Fix: Edited record function

--- public/index.php
+++ public/index.php
@@ -39,23 +39,24 @@ class csv{
 }
 
 class record{
-    public function __construct(Array $record= null)
+    public function __construct(Array $fieldNames= null, $values = null)
     {
-        print_r($record);
-        $this ->createProperty();
-
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this ->createProperty($property, $value);
+        }
+        print_r($this);
     }
-    public function createProperty($name = 'first', $value='keith'){
-        $this->{$name} = $value;
+    public function createProperty($name='first', $value='keith'){
+        $this->{$name}= $value;
     }
 }
 
 class recordFactory{
-    static public function create(Array $fieldNames= null, Array $record= null) {
+    static public function create(Array $fieldNames= null, Array $values= null) {
 
-        print_r($fieldNames);
-        print_r($record);
-        //$record = new record($array);
+
+        $record = new record($fieldNames, $values);
 
         return $record;
 }
