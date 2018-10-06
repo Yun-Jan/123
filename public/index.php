5e161870dbe2ab4b2c11937bce03a0eab7c8d2e4
yunjan <yt266@njit.edu>
2018-10-06 09:15:00 -0400

    Task: Added main function

--- public/index.php
+++ public/index.php
@@ -2,66 +2,21 @@
 /**
  * Created by PhpStorm.
  * User: yunjan
- * Date: 10/5/18
- * Time: 7:21 PM
+ * Date: 10/6/18
+ * Time: 9:05 AM
  */
-main::start("example.csv");
+main::start();
 
-class main{
-    static public function start($filename){
+class main {
+    static public function start(){
+        $file = fopen("example.csv", "r");
+        while(! feof($file)){
 
-        $records = csv::getRecords($filename);
-
-    }
-}
-
-class csv{
-    static public function getRecords($filename){
-        $file = fopen($filename,"r");
-        $fieldNames = array();
-        $count = 0;
-
-        while(! feof($file))
-        {
             $record = fgetcsv($file);
-            if($count==0) {
-                $fieldNames=$record;
-            } else {
-                $records[]=recordFactory::create($fieldNames, $record);
-            }
-
-            $count++;
+            $records[]= $record;
         }
 
         fclose($file);
-        return $records;
+        print_r($records);
     }
-}
-
-class record{
-    public function __construct(Array $fieldNames= null, $values = null)
-    {
-        $record = array_combine($fieldNames, $values);
-
-        foreach ($record as $property => $value){
-            $this ->createProperty($property, $value);
-        }
-        print_r($this);
-
-
-    }
-    public function createProperty($name = 'first', $value='keith'){
-        $this->{$name} = $value;
-    }
-}
-
-class recordFactory{
-    static public function create(Array $fieldNames= null, Array $values= null) {
-
-
-        $record = new record($fieldNames, $values);
-
-        return $record;
-}
-
 }
